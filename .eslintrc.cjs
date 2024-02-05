c32cbb707bee1f5a58c0aad906569301e278f8dc
Maxim Bazuev <bazuka5801@mail.ru>
2024-02-06 02:42:13 +0300

    feat: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,51 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/essential",
-    "plugin:prettier/essential",
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:import/typescript',
+    '@vue/typescript/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
   ],
+  plugins: ['prettier', 'import', '@typescript-eslint'],
 
-  rules: {},
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      },
+    },
+  },
+
+  rules: {
+    'import/extensions': ['error', 'never', { svg: 'always' }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+  },
 };
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+
+# Linting
+**Hint:** You can see the rules following these links and modify it in your `.eslintrc.cjs`
+
+- [ESLint Plugin Vue](https://eslint.vuejs.org/user-guide/)
+- [ESLint Typescript Parser](https://typescript-eslint.io/packages/parser/)
+- [Vue ESLint Parser](https://github.com/vuejs/vue-eslint-parser#readme)
+- [ESLint Plugin Prettier](https://github.com/prettier/eslint-plugin-prettier#readme)
